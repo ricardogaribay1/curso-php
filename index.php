7fa0c1cf4cdd0b04186d040f9072d7e9d4be0b9d
ricardogaribay1 <72572413+ricardogaribay1@users.noreply.github.com>
2025-02-18 12:29:27 -0600

    Create index.php
    creacion de index.php curso de php

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+
+
+const API_URL = "https://whenisthenextmcufilm.com/api";
+#Llamadas a <Apis> comando para utilizar Apis curl "http..."
+#Inicializar una nueva sesión de cURL; ch = cURL handle
+
+$ch = curl_init(API_URL);
+// Indicar que queremos recibir el resultado de la petición y no mostarla en pantalla
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+/* Ejecutar la petición y guardamos el resultado */
+$result = curl_exec($ch);
+
+// Una alternativa sería utilizar file_get_contents
+// $result = file_get_contents(API_URL); //si solo quieres hacer un GET de una API
+
+$data = json_decode($result, true);
+
+curl_close($ch);
+var_dump($data);
+
+?>
+
+<head>
+    <title>La próxima película de Marvel</title>
+    <meta name="description" content="La próxima película de Marvel" />
+    <meta name="viewport" content="width=device-width, initial-scales=1.0" />
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
+</head>
+
+<main>
+<pre style="font-size: 8px; overflow:scroll; height: 250px;">
+    <?php var_dump($data); ?>
+</pre>
+<section>
+<h2 class="titulo">La próxima pelicula de Marvel</h2>
+
+<img src="<?= $data["poster_url"]; ?>" width="250" alt="Poster de <?= $data["title"]; ?>"
+style="border-radius: 16px" />
+</section>
+
+<hgroup>
+    <h3><?= $data["title"]; ?> se estrena en <?= $data["days_until"]; ?> días</h3>
+    <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
+    <p>La siguiente es: <?= $data["following_production"]["title"]; ?></p>
+</hgroup>
+
+</main>
+
+<style>
+
+    :root {
+        color-scheme: dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+
+    section {
+
+        justify-content: center;
+        text-align: center;
+    } 
+
+    hgroup {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+
+    .titulo {
+        display: block;
+    }
+
+</style>
